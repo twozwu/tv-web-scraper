5fd26be0b8afbc650b5ab61d643e8bd49c953f3b
Mix <honter_tribor@hotmail.com>
2022-12-17 15:50:23 +0800

    2022/12/17-update nio url

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json())
 app.use(cors());
 app.use(morgan("tiny"));
 
-const url = "http://web.niotv.com/NIO_2017_WEB/i_index.php?cont=day&grp_id=1&way=outter";
+// const url = "http://web.niotv.com/NIO_2017_WEB/i_index.php?cont=day&grp_id=1&way=outter";
 
 app.get("/", function (req, res) {
     res.json("This is my webscraper");
@@ -27,11 +27,12 @@ app.get("/", function (req, res) {
 // })
 
 app.post("/program-list", async (req, res) => {
+    // const formData = new URLSearchParams(req.body)
     // console.log(req.body);
-    const formData = new URLSearchParams(req.body)
+    const url = `http://www.niotv.com/i_index.php?cont=day&ch_id=${req.body.sch_id}&chgrp_id=1`;
     const response = await fetch(url, {
         method: "POST",
-        body: formData,
+        body: req.body,
     });
     const html = await response.text();
 
@@ -41,7 +42,7 @@ app.post("/program-list", async (req, res) => {
     const title = $(".ch_name_title > h2").text(); // 取得標題
     results.title = title;
     $(".epg_table tbody > tr").each((index, element) => {
-        const time = $(element).find("td.epg_tab_tm").text(); // 取得時間
+        const time = $(element).find("td.epg_col_time").text(); // 取得時間
         const show = $(element).find("td > a").text(); // 取得節目
 
         results.list.push({
